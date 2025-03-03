385197e5051ec5d6ffcb7108da292ba400009a9b
amjedab@gmail.com <amjedgit1@>
2025-03-03 08:17:05 -0600

    string localisation

--- app/src/main/java/com/cricut/quizapp/model/Question.kt
+++ app/src/main/java/com/cricut/quizapp/model/Question.kt
@@ -5,26 +5,4 @@ data class Question(
   val options: List<String> = emptyList(),
   val answer: Any,
   val type: QuestionType,
-) {
-
-  fun isValidAnswer(selectedAnswer: Any): Boolean {
-    return when (type) {
-      QuestionType.TRUE_FALSE -> {
-        answer is Boolean && selectedAnswer is Boolean && answer == selectedAnswer
-      }
-
-      QuestionType.SINGLE_CHOICE -> {
-        answer is Int && selectedAnswer is Int && answer == selectedAnswer
-      }
-
-      QuestionType.MULTIPLE_CHOICE -> {
-        answer is Set<*> && selectedAnswer is Set<*> && answer == selectedAnswer
-      }
-
-      QuestionType.TEXT_INPUT -> {
-        answer is String && selectedAnswer is String &&
-            answer.trim().equals(selectedAnswer.trim(), ignoreCase = true)
-      }
-    }
-  }
-}
+)
